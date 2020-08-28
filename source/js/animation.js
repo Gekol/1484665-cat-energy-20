a1839c33ac9a7b912777014490453aad33a16613
George <[Your>
2020-08-28 14:53:43 +0300

    mobile version cats fixed

--- source/js/animation.js
+++ source/js/animation.js
@@ -41,11 +41,9 @@ if (programFormSubmitButton != null) {
 
 function hideCat(event) {
   fatCat.style.width = (width * (100 - rangeInput.value) / 100) + "px";
-  console.log("calc(50% - " + width + "px/2)");
   fatCat.style.left = "calc(50% - " + width + "px/2)";
   slimCat.style.width = (width * rangeInput.value / 100) + "px";
   slimCat.style.right = "calc(50% - " + width + "px/2)";
-  console.log(slimCat.children[slimCat.children.length - 1]);
   slimCat.children[slimCat.children.length - 1].style.position = "relative";
   slimCat.children[slimCat.children.length - 1].style.right = width * (100 - rangeInput.value) / 100 + "px";
 }
@@ -56,6 +54,28 @@ let slimCat = document.querySelector(".cat__after");
 let width = window.innerWidth < 768?320:690;
 
 if (rangeInput != null) {
+  if (window.innerWidth < 768) {
+    rangeInput.value = 0;
+  }
   hideCat(null);
-  rangeInput.addEventListener("input", hideCat)
+  rangeInput.addEventListener("input", hideCat);
+  let buttonBefore = document.querySelector(".bar__button__before");
+  if (buttonBefore != null) {
+    buttonBefore.addEventListener("click", function(event) {
+      event.preventDefault();
+      rangeInput.value = 0;
+      hideCat();
+    })
+  }
+  let buttonAfter = document.querySelector(".bar__button__after");
+  if (buttonAfter != null) {
+    buttonAfter.addEventListener("click", function(event) {
+      event.preventDefault();
+      rangeInput.value = 100;
+      hideCat();
+    })
+  }
 }
+
+
+
